b67d8a563552340fe705f28cc59c0ce859bc224b
hirohiro-sys <katohiro8843@icloud.com>
2024-08-30 16:50:35 +0900

    signinページに背景画像を追加

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Card,
   CardBody,
@@ -16,10 +17,11 @@ import {
 import firebaseServices from "../firebase";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
-import { RiShieldKeyholeFill } from "react-icons/ri";
+import { FaDoorOpen } from "react-icons/fa6";
+// import { RiShieldKeyholeFill } from "react-icons/ri";
 
 export const SignIn = () => {
-  const {auth} = firebaseServices;
+  const { auth } = firebaseServices;
   const signInWithGoogle = (): Promise<UserCredential> => {
     const provider = new GoogleAuthProvider();
     return signInWithPopup(auth, provider);
@@ -30,27 +32,41 @@ export const SignIn = () => {
   };
   return (
     <>
-    <Center h="90vh">
-      <Card p="10" maxW="lg" boxShadow="lg">
-        <CardHeader>
+      <Center h="100vh" position="relative" overflow="hidden">
+        <Box
+          position="absolute"
+          top="0"
+          left="0"
+          right="0"
+          bottom="0"
+          bgImage={`url(https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/3279945/533e08f0-f2da-d547-bfcc-276c3123c0ec.jpeg)`}
+          bgSize="cover"
+          bgPosition="center"
+          filter="brightness(70%)"
+          zIndex="-1"
+        />
+        <Card p="10" mb="20px" maxW="lg" boxShadow="lg">
+          <CardHeader>
             <Stack align="center">
-              <RiShieldKeyholeFill size="30px" />
+              {/* <RiShieldKeyholeFill size="30px" /> */}
+              <FaDoorOpen size="30px" />
+
               <Heading size="lg">Sign in</Heading>
             </Stack>
-        </CardHeader>
-        <CardBody>
-          <Stack>
-            <Button onClick={signInWithGoogle} fontSize="20px" p="25px">
-              <FcGoogle size="30px" style={{ marginRight: "8px" }} />
-              Googleでログイン
-            </Button>
-            <Button onClick={signInWithGithub} fontSize="20px" p="25px">
-              <FaGithub size="30px" style={{ marginRight: "8px" }} />
-              Githubでログイン
-            </Button>
-          </Stack>
-        </CardBody>
-      </Card>
+          </CardHeader>
+          <CardBody>
+            <Stack>
+              <Button onClick={signInWithGoogle} fontSize="20px" p="25px">
+                <FcGoogle size="30px" style={{ marginRight: "8px" }} />
+                Googleでログイン
+              </Button>
+              <Button onClick={signInWithGithub} fontSize="20px" p="25px">
+                <FaGithub size="30px" style={{ marginRight: "8px" }} />
+                Githubでログイン
+              </Button>
+            </Stack>
+          </CardBody>
+        </Card>
       </Center>
     </>
   );
