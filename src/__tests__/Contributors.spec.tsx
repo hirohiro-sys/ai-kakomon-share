359c4f87f44e93684cb7ea061ed42f550721e1ae
hirohiro-sys <katohiro8843@icloud.com>
2024-09-18 20:18:56 +0900

    なぜかエラー

--- src/__tests__/Contributors.spec.tsx
+++ src/__tests__/Contributors.spec.tsx
@@ -1,9 +1,10 @@
-import { render,screen} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Contributors } from "../components/Contributors";
 import { act } from "react";
+import userEvent from "@testing-library/user-event";
 
-
+// firebase関数のモック化
 jest.mock("firebase/app", () => ({
   initializeApp: jest.fn(),
 }));
@@ -12,6 +13,22 @@ jest.mock("firebase/auth", () => ({
     currentUser: { uid: "testUserId", email: "test@example.com" },
   }),
 }));
+
+const mockGetSubjects = jest
+  .fn()
+  .mockResolvedValue([{ id: 1, year: 1, name: "離散数学" }]);
+
+// supabase関数のモック化
+jest.mock("../lib/supabasefunctions", () => {
+  return {
+    getSubjects: () => mockGetSubjects(),
+    getSubjectUserIds: jest.fn(),
+    getUserInfo: jest.fn(),
+    //     addUser: jest.fn(),
+    //     updateUserInfo: jest.fn(),
+    //     addUserToSubject: jest.fn()
+  };
+});
 
 describe("過去問の募集投稿ページのテスト", () => {
   beforeEach(() => {
@@ -28,17 +45,17 @@ describe("過去問の募集投稿ページのテスト", () => {
 
   test("バリデーションエラーが表示される", async () => {
     const postButton = screen.getByTestId("kakomon-register-modal");
-    
+
     await act(async () => {
       postButton.click();
     });
-  
+
     const submitButton = await screen.findByTestId("kakomon-register-button");
-    
+
     await act(async () => {
       submitButton.click();
     });
-  
+
     expect(
       await screen.findByText("⚠️お名前は必須入力項目です。")
     ).toBeInTheDocument();
@@ -50,4 +67,31 @@ describe("過去問の募集投稿ページのテスト", () => {
     ).toBeInTheDocument();
   });
 
+  test("過去問を登録できる", async () => {
+    const postButton = screen.getByTestId("kakomon-register-modal");
+    await act(async () => {
+      postButton.click();
+    });
+    const submitButton = await screen.findByTestId("kakomon-register-button");
+    expect(submitButton).toBeInTheDocument();
+
+    const nameInput = await screen.findByTestId("name-input");
+    await act(async () => {
+      await userEvent.type(nameInput, "テスト太郎");
+    });
+    const kakaoIdInput = screen.getByTestId("kakaoId-input");
+    await act(async () => {
+      await userEvent.type(kakaoIdInput, "test-id");
+    });
+
+    const subjectNameInput = screen.getByTestId("subjectName-input");
+    await act(async () => {
+      await userEvent.type(subjectNameInput, "離散数学");
+    });
+    await act(async () => {
+      submitButton.click();
+    });
+
+  });
+    
 });
